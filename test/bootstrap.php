15758933555cf69802e5031b81f23d246156abfb
Miguel Muscat <miguelmuscat93@gmail.com>
2016-12-30 17:52:31 +0100

    Fixed test bootstrap file

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -5,5 +5,4 @@ error_reporting(E_ALL | E_STRICT);
 // Set timezone
 date_default_timezone_set('UTC');
 
-// Load autoloader
-require_once implode(DIRECTORY_SEPARATOR, array(dirname(__FILE__), '..', 'vendor', 'autoload.php'));
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
