225697ce3c16f870609caa6d36cc935bdd776877
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-27 16:48:35 +0100

    Updated unit test for `Booking`

--- test/unit/BookingTest.php
+++ test/unit/BookingTest.php
@@ -3,7 +3,8 @@
 namespace RebelCode\Diary\Test;
 
 use RebelCode\Diary\Booking;
-use RebelCode\Diary\DateTime;
+use RebelCode\Diary\DateTime\DateTime;
+use RebelCode\Diary\DateTime\Period;
 use Xpmock\TestCase;
 
 /**
@@ -14,59 +15,132 @@ use Xpmock\TestCase;
 class BookingTest extends TestCase
 {
     /**
-     * The instance used for testing.
-     *
-     * @since [*next-version*]
-     *
-     * @var Booking
+     * The name of the test subject.
      */
-    protected $instance;
+    const TEST_SUBJECT_CLASSNAME = '\\RebelCode\\Diary\\Booking';
 
     /**
-     * Creates an instance for testing.
+     * Creates an instance of the test subject.
      *
-     * @since [*next-version*]
+     * @param int $id    The booking ID.
+     * @param int $start The booking start timestamp.
+     * @param int $end   The booking end timestamp.
      *
-     * @return Booking
+     * @return Booking The test subject instance.
      */
-    public function createInstance()
+    public function createInstance($id, $start, $end)
     {
-        return new Booking(0, new DateTime(), new DateTime());
-    }
+        $period = new Period(
+            DateTime::createFromTimestamp($start),
+            DateTime::createFromTimestamp($end)
+        );
+        $mock = $this->mock(static::TEST_SUBJECT_CLASSNAME)
+            ->new($id, $period);
 
-    /**
-     * {@inheritdoc}
-     *
-     * @since [*next-version*]
-     */
-    public function setUp()
-    {
-        $this->instance = $this->createInstance();
+        return $mock;
     }
 
     /**
      * Tests the ID getter method to assert if the correct value is returned.
      *
      * @since [*next-version*]
-     * @covers \RebelCode\Diary\Booking::getId
      */
     public function testGetId()
     {
-        $this->reflect($this->instance)->id = 50;
+        $subject = $this->createInstance(0, 0, 0);
 
-        $this->assertEquals(50, $this->instance->getId());
+        $subject->this()->id = 50;
+
+        $this->assertEquals(50, $subject->getId());
     }
 
     /**
      * Tests the ID setter method to assert if the correct value is assigned.
      *
      * @since [*next-version*]
-     * @covers \RebelCode\Diary\Booking::setId
      */
     public function testSetId()
     {
-        $this->instance->setId(50);
+        $subject = $this->createInstance(0, 0, 0);
 
-        $this->assertEquals(50, $this->reflect($this->instance)->id);
+        $subject->setId(50);
+
+        $this->assertEquals(50, $subject->this()->id);
+    }
+
+    /**
+     * Tests the ID getter method to assert if the correct value is returned.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetPeriod()
+    {
+        $subject = $this->createInstance(0, 0, 0);
+
+        $subject->this()->period = new Period(
+            DateTime::createFromTimestamp(500),
+            DateTime::createFromTimestamp(900)
+        );
+
+        $equal = $subject->getPeriod()->getStart()->getTimestamp() === 500 &&
+            $subject->getPeriod()->getEnd()->getTimestamp() === 900;
+
+        $this->assertTrue($equal);
+    }
+
+    /**
+     * Tests the ID setter method to assert if the correct value is assigned.
+     *
+     * @since [*next-version*]
+     */
+    public function testSetPeriod()
+    {
+        $subject = $this->createInstance(0, 0, 0);
+
+        $subject->setPeriod(new Period(
+            DateTime::createFromTimestamp(500),
+            DateTime::createFromTimestamp(900)
+        ));
+
+        $equal = $subject->getPeriod()->getStart()->getTimestamp() === 500 &&
+            $subject->getPeriod()->getEnd()->getTimestamp() === 900;
+
+        $this->assertTrue($equal);
+    }
+
+    /**
+     * Tests the protected data getter method to assert if the correct data is returned.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetData()
+    {
+        $subject = $this->createInstance(50, 100, 300);
+
+        $expected = array(
+            'id'    => 50,
+            'start' => $subject->getPeriod()->getStart(),
+            'end'   => $subject->getPeriod()->getEnd(),
+        );
+
+        $this->assertEquals($expected, $subject->this()->_getData());
+    }
+
+    /**
+     * Tests the changeset getter method to assert if returned changeset contains correct data.
+     *
+     * @since [*next-version*]
+     */
+    public function testGetChangeset()
+    {
+        $subject = $this->createInstance(50, 100, 300);
+
+        $expected = array(
+            'id'    => 50,
+            'start' => $subject->getPeriod()->getStart(),
+            'end'   => $subject->getPeriod()->getEnd(),
+        );
+
+        $this->assertEquals($expected, $subject->getChangeset()->getChanges());
     }
 }
