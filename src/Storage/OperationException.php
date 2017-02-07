d3529e175178500fc55a73fdcffb6927c90bb1d0
Miguel Muscat <miguelmuscat93@gmail.com>
2017-02-07 09:48:58 +0100

    Added `OperationException`

---
+++ src/Storage/OperationException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RebelCode\Diary\Storage;
+
+/**
+ * An exception thrown when a storage operation fails.
+ *
+ * @since [*next-version*]
+ */
+class OperationException extends Exception
+{
+}
